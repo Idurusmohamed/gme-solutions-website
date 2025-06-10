15180f4932333497c78226628a027ce0ccc841d0
idurusmohamed <idriss7idriss@gmail.com>
2025-06-10 05:43:09 -0400

    Refactor Layout component by removing unused Navigation import and comments for clarity

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -2,20 +2,16 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
 import Header from './Header';
-import Navigation from './Navigation';
+// import Navigation from './Navigation'; // REMOVE THIS IMPORT
 import Footer from './Footer';
 
 const Layout = () => {
   return (
-    // This div wraps the entire layout. No width constraints here.
-    <div className="flex flex-col min-h-screen"> 
+    <div className="flex flex-col min-h-screen">
       <Header />
-      {/* Navigation is still sticky, but will appear below the absolute header on the homepage.
-          For other pages, it will be at the top. */}
-      <Navigation />
-      {/* The main content area. Page sections should define their own full-width backgrounds. */}
-      <main className="flex-grow"> 
-        <Outlet /> 
+      {/* <Navigation /> REMOVE THIS LINE */}
+      <main className="flex-grow">
+        <Outlet />
       </main>
       <Footer />
     </div>
