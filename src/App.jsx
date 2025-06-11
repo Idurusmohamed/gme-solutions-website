9aac3500c5b4d64dcc188c2422dacb25eaf8ddef
idurusmohamed <idriss7idriss@gmail.com>
2025-06-11 00:15:06 -0400

    Remove routing logic from App component; update to reflect that routing is now handled in main.jsx.

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,9 @@
-import React from 'react';
-// 1. IMPORT ScrollRestoration ALONG WITH THE OTHER ROUTER COMPONENTS
-import { BrowserRouter as Router, Routes, Route, ScrollRestoration } from 'react-router-dom'; 
-
-import Layout from './components/Layout';
-import HomePage from './pages/HomePage';
-import ServicesPage from './pages/ServicesPage';
-import AboutPage from './pages/AboutPage';
-import RequestQuotePage from './pages/RequestQuotePage';
-import ContactPage from './pages/ContactPage';
-import PrivacyPolicyPage from './pages/PrivacyPolicyPage';
-import FAQPage from './pages/FAQPage';
-// 2. We no longer need to import our custom ScrollToTop component
-
-import './App.css'; 
+// This file is no longer used for routing.
+// The routing is now handled in main.jsx using createBrowserRouter.
 
 function App() {
-  return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<HomePage />} />
-          <Route path="services" element={<ServicesPage />} />
-          <Route path="about" element={<AboutPage />} />
-          <Route path="quote" element={<RequestQuotePage />} />
-          <Route path="contact" element={<ContactPage />} />
-          <Route path="privacy-policy" element={<PrivacyPolicyPage />} />
-          <Route path="faq" element={<FAQPage />} />
-        </Route>
-      </Routes>
-      <ScrollRestoration /> {/* <-- 3. ADD THIS COMPONENT AT THE END */}
-    </Router>
-  );
+  // You can leave this empty or use it for other global providers if needed in the future.
+  return null;
 }
 
 export default App;
